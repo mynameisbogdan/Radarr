449b28f8ccc36bb14d6d1ded6629d0fe99e4eadb
felizk <felizk@gmail.com>
2026-03-01 18:02:13 +0100

    New: Calculate custom score using renamed filename before importing
    (cherry picked from commit 33fb0a4e880a5a5a3dc3d04e26d85b0a22c73552)

--- src/NzbDrone.Core/Parser/Model/LocalMovie.cs
+++ src/NzbDrone.Core/Parser/Model/LocalMovie.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using NzbDrone.Common.Extensions;
 using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.Download;
 using NzbDrone.Core.Languages;
@@ -11,11 +13,6 @@ namespace NzbDrone.Core.Parser.Model
 {
     public class LocalMovie
     {
-        public LocalMovie()
-        {
-            CustomFormats = new List<CustomFormat>();
-        }
-
         public string Path { get; set; }
         public long Size { get; set; }
         public ParsedMovieInfo FileMovieInfo { get; set; }
@@ -34,11 +31,14 @@ namespace NzbDrone.Core.Parser.Model
         public string Edition { get; set; }
         public string SceneName { get; set; }
         public bool OtherVideoFiles { get; set; }
-        public List<CustomFormat> CustomFormats { get; set; }
+        public List<CustomFormat> CustomFormats { get; set; } = new();
         public int CustomFormatScore { get; set; }
+        public List<CustomFormat> OriginalFileNameCustomFormats { get; set; } = new();
+        public int OriginalFileNameCustomFormatScore { get; set; }
         public GrabbedReleaseInfo Release { get; set; }
         public bool ScriptImported { get; set; }
         public string FileNameBeforeRename { get; set; }
+        public string FileNameUsedForCustomFormatCalculation { get; set; }
         public bool ShouldImportExtras { get; set; }
         public List<string> PossibleExtraFiles { get; set; }
         public SubtitleTitleInfo SubtitleInfo { get; set; }
@@ -46,6 +46,70 @@ namespace NzbDrone.Core.Parser.Model
         public override string ToString()
         {
             return Path;
+        }
+
+        public string GetSceneOrFileName()
+        {
+            if (SceneName.IsNotNullOrWhiteSpace())
+            {
+                return SceneName;
+            }
+
+            if (Path.IsNotNullOrWhiteSpace())
+            {
+                return System.IO.Path.GetFileNameWithoutExtension(Path);
+            }
+
+            return string.Empty;
+        }
+
+        public MovieFile ToMovieFile()
+        {
+            var movieFile = new MovieFile
+            {
+                DateAdded = DateTime.UtcNow,
+                MovieId = Movie.Id,
+                Path = Path.CleanFilePath(),
+                Quality = Quality,
+                MediaInfo = MediaInfo,
+                Movie = Movie,
+                ReleaseGroup = ReleaseGroup,
+                Edition = Edition,
+                Languages = Languages,
+                IndexerFlags = IndexerFlags,
+                SceneName = SceneName,
+                OriginalFilePath = GetOriginalFilePath()
+            };
+
+            if (Movie.Path.IsParentPath(movieFile.Path))
+            {
+                movieFile.RelativePath = Movie.Path.GetRelativePath(Path.CleanFilePath());
+            }
+
+            return movieFile;
+        }
+
+        private string GetOriginalFilePath()
+        {
+            if (FolderMovieInfo != null)
+            {
+                var folderPath = Path.GetAncestorPath(FolderMovieInfo.OriginalTitle);
+
+                if (folderPath != null)
+                {
+                    return folderPath.GetParentPath().GetRelativePath(Path);
+                }
+            }
+
+            var parentPath = Path.GetParentPath();
+            var grandparentPath = parentPath.GetParentPath();
+
+            if (grandparentPath != null)
+            {
+                return grandparentPath.GetRelativePath(Path);
+            }
+
+            return System.IO.Path.GetFileName(Path);
         }
     }
 }
