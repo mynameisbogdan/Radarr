1548725f32faa53f366b97e0bb104181c6b6f595
Bogdan <mynameisbogdan@users.noreply.github.com>
2026-04-26 13:52:29 +0300

    TypedResults for API v3 endpoints

--- src/Radarr.Api.V3/Movies/AlternativeTitleController.cs
+++ src/Radarr.Api.V3/Movies/AlternativeTitleController.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Core.Movies;
 using NzbDrone.Core.Movies.AlternativeTitles;
@@ -25,20 +27,21 @@ namespace Radarr.Api.V3.Movies
         }
 
         [HttpGet]
-        public List<AlternativeTitleResource> GetAltTitles(int? movieId, int? movieMetadataId)
+        [Produces("application/json")]
+        public Ok<List<AlternativeTitleResource>> GetAltTitles(int? movieId, int? movieMetadataId)
         {
             if (movieMetadataId.HasValue)
             {
-                return _altTitleService.GetAllTitlesForMovieMetadata(movieMetadataId.Value).ToResource();
+                return TypedResults.Ok(_altTitleService.GetAllTitlesForMovieMetadata(movieMetadataId.Value).ToResource());
             }
 
             if (movieId.HasValue)
             {
                 var movie = _movieService.GetMovie(movieId.Value);
-                return _altTitleService.GetAllTitlesForMovieMetadata(movie.MovieMetadataId).ToResource();
+                return TypedResults.Ok(_altTitleService.GetAllTitlesForMovieMetadata(movie.MovieMetadataId).ToResource());
             }
 
-            return _altTitleService.GetAllTitles().ToResource();
+            return TypedResults.Ok(_altTitleService.GetAllTitles().ToResource());
         }
     }
 }
