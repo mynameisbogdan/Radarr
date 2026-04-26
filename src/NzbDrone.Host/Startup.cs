7e717d6a2460341040606561a90ae2cb6d8d76fe
Bogdan <mynameisbogdan@users.noreply.github.com>
2026-04-26 14:04:31 +0300

    Compact JSON responses for API endpoints

--- src/NzbDrone.Host/Startup.cs
+++ src/NzbDrone.Host/Startup.cs
@@ -96,6 +96,10 @@ namespace NzbDrone.Host
             services.ConfigureHttpJsonOptions(options =>
             {
                 STJson.ApplySerializerSettings(options.SerializerOptions);
+
+#if !DEBUG
+                options.SerializerOptions.WriteIndented = false;
+#endif
             });
 
             services.AddSwaggerGen(c =>
