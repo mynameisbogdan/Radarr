Sonarr/Sonarr

New: Calculate custom score using renamed filename before importing

src/NzbDrone.Core/CustomFormats/CustomFormatCalculationService.cs
@@ -19,7 +19,7 @@ namespace NzbDrone.Core.CustomFormats
         List<CustomFormat> ParseCustomFormat(EpisodeFile episodeFile);
         List<CustomFormat> ParseCustomFormat(Blocklist blocklist, Series series);
         List<CustomFormat> ParseCustomFormat(EpisodeHistory history, Series series);
-        List<CustomFormat> ParseCustomFormat(LocalEpisode localEpisode);
+        List<CustomFormat> ParseCustomFormat(LocalEpisode localEpisode, string fileName);
     }
 
     public class CustomFormatCalculationService : ICustomFormatCalculationService
@@ -114,12 +114,12 @@ namespace NzbDrone.Core.CustomFormats
             return ParseCustomFormat(input);
         }
 
-        public List<CustomFormat> ParseCustomFormat(LocalEpisode localEpisode)
+        public List<CustomFormat> ParseCustomFormat(LocalEpisode localEpisode, string fileName)
         {
             var episodeInfo = new ParsedEpisodeInfo
             {
                 SeriesTitle = localEpisode.Series.Title,
-                ReleaseTitle = localEpisode.SceneName.IsNotNullOrWhiteSpace() ? localEpisode.SceneName : Path.GetFileName(localEpisode.Path),
+                ReleaseTitle = localEpisode.SceneName.IsNotNullOrWhiteSpace() ? localEpisode.SceneName : fileName,
                 Quality = localEpisode.Quality,
                 Languages = localEpisode.Languages,
                 ReleaseGroup = localEpisode.ReleaseGroup
@@ -133,7 +133,7 @@ namespace NzbDrone.Core.CustomFormats
                 Languages = localEpisode.Languages,
                 IndexerFlags = localEpisode.IndexerFlags,
                 ReleaseType = localEpisode.ReleaseType,
-                Filename = Path.GetFileName(localEpisode.Path)
+                Filename = fileName
             };
 
             return ParseCustomFormat(input);

mynameisbogdan/Radarr

(cherry picked from commit 33fb0a4e880a5a5a3dc3d04e26d85b0a22c73552)

src/NzbDrone.Core/CustomFormats/CustomFormatCalculationService.cs
@@ -20,7 +20,7 @@ namespace NzbDrone.Core.CustomFormats
         List<CustomFormat> ParseCustomFormat(MovieFile movieFile);
         List<CustomFormat> ParseCustomFormat(Blocklist blocklist, Movie movie);
         List<CustomFormat> ParseCustomFormat(MovieHistory history, Movie movie);
-        List<CustomFormat> ParseCustomFormat(LocalMovie localMovie);
+        List<CustomFormat> ParseCustomFormat(LocalMovie localMovie, string fileName);
     }
 
     public class CustomFormatCalculationService : ICustomFormatCalculationService
@@ -115,12 +115,12 @@ namespace NzbDrone.Core.CustomFormats
             return ParseCustomFormat(input);
         }
 
-        public List<CustomFormat> ParseCustomFormat(LocalMovie localMovie)
+        public List<CustomFormat> ParseCustomFormat(LocalMovie localMovie, string fileName)
         {
             var movieInfo = new ParsedMovieInfo
             {
                 MovieTitles = new List<string>() { localMovie.Movie.Title },
-                SimpleReleaseTitle = localMovie.SceneName.IsNotNullOrWhiteSpace() ? localMovie.SceneName.SimplifyReleaseTitle() : Path.GetFileName(localMovie.Path).SimplifyReleaseTitle(),
+                SimpleReleaseTitle = localMovie.SceneName.IsNotNullOrWhiteSpace() ? localMovie.SceneName.SimplifyReleaseTitle() : fileName.SimplifyReleaseTitle(),
                 ReleaseTitle = localMovie.SceneName,
                 Quality = localMovie.Quality,
                 Edition = localMovie.Edition,
@@ -135,7 +135,7 @@ namespace NzbDrone.Core.CustomFormats
                 Size = localMovie.Size,
                 Languages = localMovie.Languages,
                 IndexerFlags = localMovie.IndexerFlags,
-                Filename = Path.GetFileName(localMovie.Path)
+                Filename = fileName
             };
 
             return ParseCustomFormat(input);