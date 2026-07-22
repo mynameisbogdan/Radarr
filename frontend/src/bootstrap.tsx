27ba20f77e5c9108968d67c20148e043d3963af9
Bogdan <mynameisbogdan@users.noreply.github.com>
2026-07-22 14:44:58 +0300

    Guard creating the react root once

--- frontend/src/bootstrap.tsx
+++ frontend/src/bootstrap.tsx
@@ -1,16 +1,22 @@
 import { createBrowserHistory } from 'history';
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot, Root } from 'react-dom/client';
 import createAppStore from 'Store/createAppStore';
 import App from './App/App';
 
 import 'Diag/ConsoleApi';
 
+let root: Root | null = null;
+
 export async function bootstrap() {
   const history = createBrowserHistory();
   const store = createAppStore(history);
-  const container = document.getElementById('root');
 
-  const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+  const container = document.getElementById('root');
+  if (!container) {
+    throw new Error('Missing #root element');
+  }
+
+  root ??= createRoot(container);
   root.render(<App store={store} history={history} />);
 }
