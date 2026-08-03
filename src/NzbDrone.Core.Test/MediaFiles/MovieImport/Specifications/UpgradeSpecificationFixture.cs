Sonarr/Sonarr

New: Calculate custom score using renamed filename before importing

src/NzbDrone.Core.Test/MediaFiles/EpisodeImport/Specifications/UpgradeSpecificationFixture.cs
@@ -9,6 +9,7 @@ using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.Datastore;
 using NzbDrone.Core.Languages;
 using NzbDrone.Core.MediaFiles;
+using NzbDrone.Core.MediaFiles.EpisodeImport;
 using NzbDrone.Core.MediaFiles.EpisodeImport.Specifications;
 using NzbDrone.Core.Parser.Model;
 using NzbDrone.Core.Profiles;
@@ -564,6 +565,49 @@ namespace NzbDrone.Core.Test.MediaFiles.EpisodeImport.Specifications
                 .ToList();
 
             Subject.IsSatisfiedBy(_localEpisode, null).Accepted.Should().BeFalse();
+        }
+
+        [Test]
+        public void should_return_false_and_a_specific_reason_if_not_upgrade_to_custom_format_score_after_local_file_rename_but_was_before()
+        {
+            var episodeFileCustomFormats = Builder<CustomFormat>.CreateListOfSize(1).Build().ToList();
+
+            var episodeFile = new EpisodeFile
+            {
+                Quality = new QualityModel(Quality.Bluray1080p)
+            };
+
+            _series.QualityProfile.Value.FormatItems = episodeFileCustomFormats.Select(c => new ProfileFormatItem
+                {
+                    Format = c,
+                    Score = 50
+                })
+                .ToList();
+
+            Mocker.GetMock<IConfigService>()
+                .Setup(s => s.DownloadPropersAndRepacks)
+                .Returns(ProperDownloadTypes.DoNotPrefer);
+
+            Mocker.GetMock<ICustomFormatCalculationService>()
+                .Setup(s => s.ParseCustomFormat(episodeFile))
+                .Returns(episodeFileCustomFormats);
+
+            _localEpisode.Quality = new QualityModel(Quality.Bluray1080p);
+            _localEpisode.CustomFormats = Builder<CustomFormat>.CreateListOfSize(1).Build().ToList();
+            _localEpisode.CustomFormatScore = 20;
+            _localEpisode.OriginalFileNameCustomFormats = Builder<CustomFormat>.CreateListOfSize(1).Build().ToList();
+            _localEpisode.OriginalFileNameCustomFormatScore = 60;
+
+            _localEpisode.Episodes = Builder<Episode>.CreateListOfSize(1)
+                .All()
+                .With(e => e.EpisodeFileId = 1)
+                .With(e => e.EpisodeFile = new LazyLoaded<EpisodeFile>(episodeFile))
+                .Build()
+                .ToList();
+
+            var result = Subject.IsSatisfiedBy(_localEpisode, null);
+            result.Accepted.Should().BeFalse();
+            result.Reason.Should().Be(ImportRejectionReason.NotCustomFormatUpgradeAfterRename);
         }
     }
 }

src/NzbDrone.Core/CustomFormats/CustomFormatCalculationService.cs
@@ -19,7 +19,7 @@ namespace NzbDrone.Core.CustomFormats
         List<CustomFormat> ParseCustomFormat(EpisodeFile episodeFile);
         List<CustomFormat> ParseCustomFormat(Blocklist blocklist, Series series);
         List<CustomFormat> ParseCustomFormat(EpisodeHistory history, Series series);
-        List<CustomFormat> ParseCustomFormat(LocalEpisode localEpisode);
+        List<CustomFormat> ParseCustomFormat(LocalEpisode localEpisode, string fileName);
     }
 
     public class CustomFormatCalculationService : ICustomFormatCalculationService
@@ -114,12 +114,12 @@ namespace NzbDrone.Core.CustomFormats
             return ParseCustomFormat(input);
         }
 
-        public List<CustomFormat> ParseCustomFormat(LocalEpisode localEpisode)
+        public List<CustomFormat> ParseCustomFormat(LocalEpisode localEpisode, string fileName)
         {
             var episodeInfo = new ParsedEpisodeInfo
             {
                 SeriesTitle = localEpisode.Series.Title,
-                ReleaseTitle = localEpisode.SceneName.IsNotNullOrWhiteSpace() ? localEpisode.SceneName : Path.GetFileName(localEpisode.Path),
+                ReleaseTitle = localEpisode.SceneName.IsNotNullOrWhiteSpace() ? localEpisode.SceneName : fileName,
                 Quality = localEpisode.Quality,
                 Languages = localEpisode.Languages,
                 ReleaseGroup = localEpisode.ReleaseGroup
@@ -133,7 +133,7 @@ namespace NzbDrone.Core.CustomFormats
                 Languages = localEpisode.Languages,
                 IndexerFlags = localEpisode.IndexerFlags,
                 ReleaseType = localEpisode.ReleaseType,
-                Filename = Path.GetFileName(localEpisode.Path)
+                Filename = fileName
             };
 
             return ParseCustomFormat(input);

src/NzbDrone.Core/MediaFiles/MediaInfo/UpdateMediaInfoService.cs
@@ -85,7 +85,12 @@ namespace NzbDrone.Core.MediaFiles.MediaInfo
             }
 
             episodeFile.MediaInfo = updatedMediaInfo;
-            _mediaFileService.Update(episodeFile);
+
+            if (episodeFile.Id != 0)
+            {
+                _mediaFileService.Update(episodeFile);
+            }
+
             _logger.Debug("Updated MediaInfo for '{0}'", path);
 
             return true;

src/NzbDrone.Core/MediaFiles/EpisodeImport/Specifications/UpgradeSpecification.cs
@@ -62,6 +62,8 @@ namespace NzbDrone.Core.MediaFiles.EpisodeImport.Specifications
                 var currentFormatScore = qualityProfile.CalculateCustomFormatScore(currentFormats);
                 var newFormats = localEpisode.CustomFormats;
                 var newFormatScore = localEpisode.CustomFormatScore;
+                var newFormatsBeforeRename = localEpisode.OriginalFileNameCustomFormats;
+                var newFormatScoreBeforeRename = localEpisode.OriginalFileNameCustomFormatScore;
 
                 if (qualityCompare == 0 && newFormatScore < currentFormatScore)
                 {
@@ -70,6 +72,18 @@ namespace NzbDrone.Core.MediaFiles.EpisodeImport.Specifications
                         newFormatScore,
                         currentFormats != null ? currentFormats.ConcatToString() : "",
                         currentFormatScore);
+
+                    if (newFormatScoreBeforeRename > currentFormatScore)
+                    {
+                        return ImportSpecDecision.Reject(ImportRejectionReason.NotCustomFormatUpgradeAfterRename,
+                            "Not a Custom Format upgrade for existing episode file(s). AfterRename: [{0}] ({1}) do not improve on Existing: [{2}] ({3}) even though BeforeRename: [{4}] ({5}) did.",
+                            newFormats != null ? newFormats.ConcatToString() : "",
+                            newFormatScore,
+                            currentFormats != null ? currentFormats.ConcatToString() : "",
+                            currentFormatScore,
+                            newFormatsBeforeRename != null ? newFormatsBeforeRename.ConcatToString() : "",
+                            newFormatScoreBeforeRename);
+                    }
 
                     return ImportSpecDecision.Reject(ImportRejectionReason.NotCustomFormatUpgrade,
                         "Not a Custom Format upgrade for existing episode file(s). New: [{0}] ({1}) do not improve on Existing: [{2}] ({3})",

mynameisbogdan/Radarr

(cherry picked from commit 33fb0a4e880a5a5a3dc3d04e26d85b0a22c73552)

src/NzbDrone.Core.Test/MediaFiles/MovieImport/Specifications/UpgradeSpecificationFixture.cs
@@ -5,6 +5,7 @@ using NUnit.Framework;
 using NzbDrone.Core.Configuration;
 using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.MediaFiles;
+using NzbDrone.Core.MediaFiles.MovieImport;
 using NzbDrone.Core.MediaFiles.MovieImport.Specifications;
 using NzbDrone.Core.Movies;
 using NzbDrone.Core.Parser.Model;
@@ -236,6 +237,45 @@ namespace NzbDrone.Core.Test.MediaFiles.MovieImport.Specifications
             _localMovie.Movie.MovieFile = movieFile;
 
             Subject.IsSatisfiedBy(_localMovie, null).Accepted.Should().BeFalse();
+        }
+
+        [Test]
+        public void should_return_false_and_a_specific_reason_if_not_upgrade_to_custom_format_score_after_local_file_rename_but_was_before()
+        {
+            var movieFileCustomFormats = Builder<CustomFormat>.CreateListOfSize(1).Build().ToList();
+
+            var movieFile = new MovieFile
+            {
+                Quality = new QualityModel(Quality.Bluray1080p)
+            };
+
+            _movie.QualityProfile.FormatItems = movieFileCustomFormats.Select(c => new ProfileFormatItem
+                {
+                    Format = c,
+                    Score = 50
+                })
+                .ToList();
+
+            Mocker.GetMock<IConfigService>()
+                .Setup(s => s.DownloadPropersAndRepacks)
+                .Returns(ProperDownloadTypes.DoNotPrefer);
+
+            Mocker.GetMock<ICustomFormatCalculationService>()
+                .Setup(s => s.ParseCustomFormat(movieFile))
+                .Returns(movieFileCustomFormats);
+
+            _localMovie.Quality = new QualityModel(Quality.Bluray1080p);
+            _localMovie.CustomFormats = Builder<CustomFormat>.CreateListOfSize(1).Build().ToList();
+            _localMovie.CustomFormatScore = 20;
+            _localMovie.OriginalFileNameCustomFormats = Builder<CustomFormat>.CreateListOfSize(1).Build().ToList();
+            _localMovie.OriginalFileNameCustomFormatScore = 60;
+
+            _localMovie.Movie.MovieFileId = 1;
+            _localMovie.Movie.MovieFile = movieFile;
+
+            var result = Subject.IsSatisfiedBy(_localMovie, null);
+            result.Accepted.Should().BeFalse();
+            result.Reason.Should().Be(ImportRejectionReason.NotCustomFormatUpgradeAfterRename);
         }
     }
 }

src/NzbDrone.Core/CustomFormats/CustomFormatCalculationService.cs
@@ -20,7 +20,7 @@ namespace NzbDrone.Core.CustomFormats
         List<CustomFormat> ParseCustomFormat(MovieFile movieFile);
         List<CustomFormat> ParseCustomFormat(Blocklist blocklist, Movie movie);
         List<CustomFormat> ParseCustomFormat(MovieHistory history, Movie movie);
-        List<CustomFormat> ParseCustomFormat(LocalMovie localMovie);
+        List<CustomFormat> ParseCustomFormat(LocalMovie localMovie, string fileName);
     }
 
     public class CustomFormatCalculationService : ICustomFormatCalculationService
@@ -117,12 +117,12 @@ namespace NzbDrone.Core.CustomFormats
             return ParseCustomFormat(input);
         }
 
-        public List<CustomFormat> ParseCustomFormat(LocalMovie localMovie)
+        public List<CustomFormat> ParseCustomFormat(LocalMovie localMovie, string fileName)
         {
             var movieInfo = new ParsedMovieInfo
             {
                 MovieTitles = new List<string> { localMovie.Movie.Title },
-                SimpleReleaseTitle = localMovie.SceneName.IsNotNullOrWhiteSpace() ? localMovie.SceneName.SimplifyReleaseTitle() : Path.GetFileName(localMovie.Path).SimplifyReleaseTitle(),
+                SimpleReleaseTitle = localMovie.SceneName.IsNotNullOrWhiteSpace() ? localMovie.SceneName.SimplifyReleaseTitle() : fileName.SimplifyReleaseTitle(),
                 ReleaseTitle = localMovie.SceneName,
                 Year = localMovie.Movie.Year,
                 Quality = localMovie.Quality,
@@ -138,7 +138,7 @@ namespace NzbDrone.Core.CustomFormats
                 Size = localMovie.Size,
                 Languages = localMovie.Languages,
                 IndexerFlags = localMovie.IndexerFlags,
-                Filename = Path.GetFileName(localMovie.Path)
+                Filename = fileName
             };
 
             return ParseCustomFormat(input);

src/NzbDrone.Core/MediaFiles/MediaInfo/UpdateMediaInfoService.cs
@@ -85,7 +85,12 @@ namespace NzbDrone.Core.MediaFiles.MediaInfo
             }
 
             movieFile.MediaInfo = updatedMediaInfo;
-            _mediaFileService.Update(movieFile);
+
+            if (movieFile.Id != 0)
+            {
+                _mediaFileService.Update(movieFile);
+            }
+
             _logger.Debug("Updated MediaInfo for '{0}'", path);
 
             return true;

src/NzbDrone.Core/MediaFiles/MovieImport/Specifications/UpgradeSpecification.cs
@@ -63,6 +63,8 @@ namespace NzbDrone.Core.MediaFiles.MovieImport.Specifications
                 var currentFormatScore = qualityProfile.CalculateCustomFormatScore(currentCustomFormats);
                 var newCustomFormats = localMovie.CustomFormats;
                 var newFormatScore = localMovie.CustomFormatScore;
+                var newFormatsBeforeRename = localMovie.OriginalFileNameCustomFormats;
+                var newFormatScoreBeforeRename = localMovie.OriginalFileNameCustomFormatScore;
 
                 if (qualityCompare == 0 && newFormatScore < currentFormatScore)
                 {
@@ -71,6 +73,18 @@ namespace NzbDrone.Core.MediaFiles.MovieImport.Specifications
                         newFormatScore,
                         currentCustomFormats != null ? currentCustomFormats.ConcatToString() : "",
                         currentFormatScore);
+
+                    if (newFormatScoreBeforeRename > currentFormatScore)
+                    {
+                        return ImportSpecDecision.Reject(ImportRejectionReason.NotCustomFormatUpgradeAfterRename,
+                            "Not a Custom Format upgrade for existing movie file(s). AfterRename: [{0}] ({1}) do not improve on Existing: [{2}] ({3}) even though BeforeRename: [{4}] ({5}) did.",
+                            newCustomFormats != null ? newCustomFormats.ConcatToString() : "",
+                            newFormatScore,
+                            currentCustomFormats != null ? currentCustomFormats.ConcatToString() : "",
+                            currentFormatScore,
+                            newFormatsBeforeRename != null ? newFormatsBeforeRename.ConcatToString() : "",
+                            newFormatScoreBeforeRename);
+                    }
 
                     return ImportSpecDecision.Reject(ImportRejectionReason.NotCustomFormatUpgrade,
                         "Not a Custom Format upgrade for existing movie file(s). New: [{0}] ({1}) do not improve on Existing: [{2}] ({3})",