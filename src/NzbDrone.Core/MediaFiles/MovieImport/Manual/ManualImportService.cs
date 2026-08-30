Sonarr/Sonarr

Fixed: Ignore invalid languages during Manual Import

src/NzbDrone.Core/MediaFiles/EpisodeImport/Manual/ManualImportService.cs
@@ -542,9 +542,13 @@ namespace NzbDrone.Core.MediaFiles.EpisodeImport.Manual
                 localEpisode.Episodes = episodes;
                 localEpisode.ReleaseGroup = file.ReleaseGroup;
                 localEpisode.Quality = file.Quality;
-                localEpisode.Languages = file.Languages;
                 localEpisode.IndexerFlags = (IndexerFlags)file.IndexerFlags;
                 localEpisode.ReleaseType = file.ReleaseType;
+
+                if (file.Languages is { Count: > 0 } && file.Languages.All(l => l is not null && l.IsValid()))
+                {
+                    localEpisode.Languages = file.Languages;
+                }
 
                 _localEpisodeFormatCalculator.UpdateEpisodeCustomFormats(localEpisode);
 

mynameisbogdan/Radarr

(cherry picked from commit 143a007cec0b7e8408ffd1b8ea3282f1cfe76cb9)

src/NzbDrone.Core/MediaFiles/MovieImport/Manual/ManualImportService.cs
@@ -454,8 +454,12 @@ namespace NzbDrone.Core.MediaFiles.MovieImport.Manual
                 localMovie.Movie = movie;
                 localMovie.ReleaseGroup = file.ReleaseGroup;
                 localMovie.Quality = file.Quality;
-                localMovie.Languages = file.Languages;
                 localMovie.IndexerFlags = (IndexerFlags)file.IndexerFlags;
+
+                if (file.Languages is { Count: > 0 } && file.Languages.All(l => l is not null && l.IsValid()))
+                {
+                    localMovie.Languages = file.Languages;
+                }
 
                 _localMovieFormatCalculator.UpdateMovieCustomFormats(localMovie);
 