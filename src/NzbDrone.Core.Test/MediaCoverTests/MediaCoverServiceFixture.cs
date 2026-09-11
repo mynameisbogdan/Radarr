Sonarr/Sonarr

Fixed: Don't cache media covers that aren't downloaded

src/NzbDrone.Core.Test/MediaCoverTests/MediaCoverServiceFixture.cs
@@ -38,9 +38,75 @@ namespace NzbDrone.Core.Test.MediaCoverTests
                 new() { CoverType = MediaCoverTypes.Banner, RemoteUrl = "https://artworks.examples.com/banners/1.jpg" }
             };
 
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(true);
+
             Subject.ConvertToLocalUrls(12, covers);
 
             covers.Single().Url.Should().Be("/MediaCover/12/banner.jpg?h=a6210a45e2b93963ad9e");
+        }
+
+        [Test]
+        public void should_convert_cover_urls_to_local_without_hash_if_cover_has_not_been_downloaded()
+        {
+            var covers = new List<MediaCover.MediaCover>
+            {
+                new() { CoverType = MediaCoverTypes.Banner, RemoteUrl = "https://artworks.examples.com/banners/1.jpg" }
+            };
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(false);
+
+            Subject.ConvertToLocalUrls(12, covers);
+
+            covers.Single().Url.Should().Be("/MediaCover/12/banner.jpg");
+        }
+
+        [Test]
+        public void should_only_check_if_cover_exists_on_disk_once()
+        {
+            var covers = new List<MediaCover.MediaCover>
+            {
+                new() { CoverType = MediaCoverTypes.Banner, RemoteUrl = "https://artworks.examples.com/banners/1.jpg" }
+            };
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(true);
+
+            Subject.ConvertToLocalUrls(12, covers);
+            Subject.ConvertToLocalUrls(12, covers);
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Verify(v => v.FileExists(It.IsAny<string>()), Times.Once());
+        }
+
+        [Test]
+        public void should_add_hash_to_cover_url_once_cover_exists()
+        {
+            var covers = new List<MediaCover.MediaCover>
+            {
+                new() { CoverType = MediaCoverTypes.Poster, RemoteUrl = "https://artworks.examples.com/posters/1.jpg" }
+            };
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(false);
+
+            Subject.ConvertToLocalUrls(_series.Id, covers);
+
+            covers.Single().Url.Should().Be($"/MediaCover/{_series.Id}/poster.jpg");
+
+            Mocker.GetMock<ICoverExistsSpecification>()
+                  .Setup(v => v.AlreadyExists(It.IsAny<string>(), It.IsAny<string>()))
+                  .Returns(true);
+
+            Subject.HandleAsync(new SeriesUpdatedEvent(_series));
+            Subject.ConvertToLocalUrls(_series.Id, covers);
+
+            covers.Single().Url.Should().Be($"/MediaCover/{_series.Id}/poster.jpg?h=2a57c239a7baaae159e7");
         }
 
         [Test]

src/NzbDrone.Core/MediaCover/MediaCoverService.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Threading;
 using NLog;
+using NzbDrone.Common.Cache;
 using NzbDrone.Common.Disk;
 using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Common.Extensions;
@@ -35,6 +36,7 @@ namespace NzbDrone.Core.MediaCover
         private readonly IEventAggregator _eventAggregator;
         private readonly Logger _logger;
 
+        private readonly ICached<bool> _coverExistsCache;
         private readonly string _coverRootFolder;
 
         // ImageSharp is slow on ARM (no hardware acceleration on mono yet)
@@ -49,6 +51,7 @@ namespace NzbDrone.Core.MediaCover
                                  ICoverExistsSpecification coverExistsSpecification,
                                  IConfigFileProvider configFileProvider,
                                  IEventAggregator eventAggregator,
+                                 ICacheManager cacheManager,
                                  Logger logger)
         {
             _mediaCoverProxy = mediaCoverProxy;
@@ -60,6 +63,7 @@ namespace NzbDrone.Core.MediaCover
             _eventAggregator = eventAggregator;
             _logger = logger;
 
+            _coverExistsCache = cacheManager.GetCache<bool>(GetType(), "coverExists");
             _coverRootFolder = appFolderInfo.GetMediaCoverPath();
         }
 
@@ -91,11 +95,26 @@ namespace NzbDrone.Core.MediaCover
 
                     mediaCover.Url = _configFileProvider.UrlBase + @"/MediaCover/" + seriesId + "/" + mediaCover.CoverType.ToString().ToLowerInvariant() + GetExtension(mediaCover.CoverType);
 
-                    if (mediaCover.RemoteUrl.IsNotNullOrWhiteSpace())
+                    if (mediaCover.RemoteUrl.IsNotNullOrWhiteSpace() && CoverExists(seriesId, mediaCover.CoverType))
                     {
                         mediaCover.Url += "?h=" + mediaCover.RemoteUrl.SHA256Hash()[..20];
                     }
                 }
+            }
+        }
+
+        private bool CoverExists(int seriesId, MediaCoverTypes coverType)
+        {
+            var filePath = GetCoverPath(seriesId, coverType);
+
+            return _coverExistsCache.Get(filePath, () => _diskProvider.FileExists(filePath));
+        }
+
+        private void RemoveCoverExistsCache(Series series)
+        {
+            foreach (var cover in series.Images)
+            {
+                _coverExistsCache.Remove(GetCoverPath(series.Id, cover.CoverType));
             }
         }
 
@@ -128,6 +147,8 @@ namespace NzbDrone.Core.MediaCover
                         DownloadCover(series, cover);
                         updated = true;
                     }
+
+                    _coverExistsCache.Set(fileName, true);
                 }
                 catch (HttpException e)
                 {
@@ -235,6 +256,8 @@ namespace NzbDrone.Core.MediaCover
         {
             foreach (var series in message.Series)
             {
+                RemoveCoverExistsCache(series);
+
                 var path = GetSeriesCoverPath(series.Id);
                 if (_diskProvider.FolderExists(path))
                 {

mynameisbogdan/Radarr

src/NzbDrone.Core.Test/MediaCoverTests/MediaCoverServiceFixture.cs
@@ -38,9 +38,75 @@ namespace NzbDrone.Core.Test.MediaCoverTests
                 new() { CoverType = MediaCoverTypes.Banner, RemoteUrl = "https://artworks.examples.com/banners/1.jpg" }
             };
 
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(true);
+
             Subject.ConvertToLocalUrls(12, covers);
 
             covers.Single().Url.Should().Be("/MediaCover/12/banner.jpg?h=a6210a45e2b93963ad9e");
+        }
+
+        [Test]
+        public void should_convert_cover_urls_to_local_without_hash_if_cover_has_not_been_downloaded()
+        {
+            var covers = new List<MediaCover.MediaCover>
+            {
+                new() { CoverType = MediaCoverTypes.Banner, RemoteUrl = "https://artworks.examples.com/banners/1.jpg" }
+            };
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(false);
+
+            Subject.ConvertToLocalUrls(12, covers);
+
+            covers.Single().Url.Should().Be("/MediaCover/12/banner.jpg");
+        }
+
+        [Test]
+        public void should_only_check_if_cover_exists_on_disk_once()
+        {
+            var covers = new List<MediaCover.MediaCover>
+            {
+                new() { CoverType = MediaCoverTypes.Banner, RemoteUrl = "https://artworks.examples.com/banners/1.jpg" }
+            };
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(true);
+
+            Subject.ConvertToLocalUrls(12, covers);
+            Subject.ConvertToLocalUrls(12, covers);
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Verify(v => v.FileExists(It.IsAny<string>()), Times.Once());
+        }
+
+        [Test]
+        public void should_add_hash_to_cover_url_once_cover_exists()
+        {
+            var covers = new List<MediaCover.MediaCover>
+            {
+                new() { CoverType = MediaCoverTypes.Poster, RemoteUrl = "https://artworks.examples.com/posters/1.jpg" }
+            };
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(false);
+
+            Subject.ConvertToLocalUrls(_movie.Id, covers);
+
+            covers.Single().Url.Should().Be($"/MediaCover/{_movie.Id}/poster.jpg");
+
+            Mocker.GetMock<ICoverExistsSpecification>()
+                  .Setup(v => v.AlreadyExists(It.IsAny<string>(), It.IsAny<string>()))
+                  .Returns(true);
+
+            Subject.HandleAsync(new MovieUpdatedEvent(_movie));
+            Subject.ConvertToLocalUrls(_movie.Id, covers);
+
+            covers.Single().Url.Should().Be($"/MediaCover/{_movie.Id}/poster.jpg?h=2a57c239a7baaae159e7");
         }
 
         [Test]

src/NzbDrone.Core/MediaCover/MediaCoverService.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Threading;
 using NLog;
+using NzbDrone.Common.Cache;
 using NzbDrone.Common.Disk;
 using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Common.Extensions;
@@ -35,6 +36,7 @@ namespace NzbDrone.Core.MediaCover
         private readonly IEventAggregator _eventAggregator;
         private readonly Logger _logger;
 
+        private readonly ICached<bool> _coverExistsCache;
         private readonly string _coverRootFolder;
 
         // ImageSharp is slow on ARM (no hardware acceleration on mono yet)
@@ -49,6 +51,7 @@ namespace NzbDrone.Core.MediaCover
                                  ICoverExistsSpecification coverExistsSpecification,
                                  IConfigFileProvider configFileProvider,
                                  IEventAggregator eventAggregator,
+                                 ICacheManager cacheManager,
                                  Logger logger)
         {
             _mediaCoverProxy = mediaCoverProxy;
@@ -60,6 +63,7 @@ namespace NzbDrone.Core.MediaCover
             _eventAggregator = eventAggregator;
             _logger = logger;
 
+            _coverExistsCache = cacheManager.GetCache<bool>(GetType(), "coverExists");
             _coverRootFolder = appFolderInfo.GetMediaCoverPath();
         }
 
@@ -91,11 +95,26 @@ namespace NzbDrone.Core.MediaCover
 
                     mediaCover.Url = _configFileProvider.UrlBase + @"/MediaCover/" + movieId + "/" + mediaCover.CoverType.ToString().ToLowerInvariant() + GetExtension(mediaCover.CoverType);
 
-                    if (mediaCover.RemoteUrl.IsNotNullOrWhiteSpace())
+                    if (mediaCover.RemoteUrl.IsNotNullOrWhiteSpace() && CoverExists(movieId, mediaCover.CoverType))
                     {
                         mediaCover.Url += "?h=" + mediaCover.RemoteUrl.SHA256Hash()[..20];
                     }
                 }
+            }
+        }
+
+        private bool CoverExists(int movieId, MediaCoverTypes coverType)
+        {
+            var filePath = GetCoverPath(movieId, coverType);
+
+            return _coverExistsCache.Get(filePath, () => _diskProvider.FileExists(filePath));
+        }
+
+        private void RemoveCoverExistsCache(Movie movie)
+        {
+            foreach (var cover in movie.MovieMetadata.Value.Images)
+            {
+                _coverExistsCache.Remove(GetCoverPath(movie.Id, cover.CoverType));
             }
         }
 
@@ -128,6 +147,8 @@ namespace NzbDrone.Core.MediaCover
                         DownloadCover(movie, cover);
                         updated = true;
                     }
+
+                    _coverExistsCache.Set(fileName, true);
                 }
                 catch (HttpException e)
                 {
@@ -235,6 +256,8 @@ namespace NzbDrone.Core.MediaCover
         {
             foreach (var movie in message.Movies)
             {
+                RemoveCoverExistsCache(movie);
+
                 var path = GetMovieCoverPath(movie.Id);
                 if (_diskProvider.FolderExists(path))
                 {