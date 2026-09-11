mynameisbogdan/Sonarr

Background based on score for interactive search rows

frontend/src/InteractiveSearch/InteractiveSearchRow.css.d.ts
@@ -15,6 +15,8 @@ interface CssExports {
   'quality': string;
   'rejected': string;
   'sceneMapping': string;
+  'scoreNegative': string;
+  'scorePositive': string;
   'size': string;
   'titleContent': string;
 }

mynameisbogdan/Radarr

frontend/src/InteractiveSearch/InteractiveSearchRow.css.d.ts
@@ -12,10 +12,13 @@ interface CssExports {
   'interactiveIcon': string;
   'languages': string;
   'manualDownloadContent': string;
+  'negativeScore': string;
   'peers': string;
+  'positiveScore': string;
   'protocol': string;
   'quality': string;
   'rejected': string;
+  'row': string;
   'size': string;
   'titleContent': string;
 }